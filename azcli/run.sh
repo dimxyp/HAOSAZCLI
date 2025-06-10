eb9ef7a7e727502c70c922b4bccfd2cdcf79afc1
dimx <xypolias.d@outlook.com>
2025-06-10 12:40:20 +0300

    Update run.sh

--- azcli/run.sh
+++ azcli/run.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-echo "Starting Azure CLI Add‑on..."
+echo "Starting Azure CLI Add-on..."
 
 # Login with Service Principal using env vars from config.json
 az login --service-principal --username "$CLIENT_ID" --password "$CLIENT_SECRET" --tenant "$TENANT_ID"
+
 az account list --output table || echo "You may need to login using a service principal."
 echo "Logged in as Service Principal, ready for commands."
 
